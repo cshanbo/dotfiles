3932a6882d7ff9448257ee9b1089cdce05fe6da8
Shanbo Cheng <cshanbo@gmail.com>
2017-05-22 22:02:14 +0800

    update install

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # dotfiles
 My own vim-configuration
+
+# installation
+
+```
+cd dotfiles
+sh install.sh
+```
--- install.sh
+++ install.sh
@@ -1,6 +1,15 @@
-cp -rf .bashrc .bash_profile .vim ~/
 tar -xf ctags-5.8.tar.gz
 cd ctags-5.8
 ./configure
 make
 sudo make install
+
+mkdir ~/backup-vim
+mv ~/.vimrc ~/.bashrc ~/.bash_profile ~/.vim ~/backup-vim
+
+ln -s .bashrc ~/.bashrc
+ln -s .bash_profile ~/.bash_profile
+ln -s .vim ~/.vim
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
